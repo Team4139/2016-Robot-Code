c47b49687b712253703cf33d1b8b160d34073766
Andrew Kuo <a.kuo3@hotmail.com>
2016-02-13 13:56:06 -0800

    First output commit

--- 2016-Robot/src/Input.cpp
+++ 2016-Robot/src/Input.cpp
@@ -9,7 +9,14 @@ struct Input_In
 
 struct Input_Out
 {
-
+	//Controller
+	float returnX, returnY, returnRotation;
+	int turboMode;
+	int triggerPressed, buttonPressed;
+	//Sensors
+	float returnGyroAngle;
+	float returnAccelX, returnAccelY, returnAccelz;
+	float returnDistance;
 };
 
 class Input
@@ -25,10 +32,32 @@ public:
 	}
 	Input_Out Run(Input_In input)
 	{
+		//Structs and stuff
 		Input_Out output;
-
-		// Put all the code here
-
+		
+		X360Controller_In xbIn;
+		X360Controller_out xbOut;
+		Sensors_In sensIn;
+		Sensors_Out sensOut;
+		
+		sensOut = sensors->Run(sensIn);
+		xbOut = controller->Run(xbIn);
+		
+		//Retreving controller info
+		output.returnX = xbOut.returnX;
+		output.returnY = xbOut.returnY;
+		output.returnRotation = xbOut.returnRotation;
+		output.returnTurbo = xbOut.returnTurbo;
+		output.returnTriggerPressed = xbOut.returnTriggerPressed;
+		output.returnButtonPressed = xbOut.returnButtonPressed;
+		
+		//Retreving sensor info
+		output.returnGyroAngle = sensOut.returnGyroAngle;
+		output.returnAccelX = sensOut.returnAccelX;
+		output.returnAccelY = sensOut.returnAccelY;
+		output.returnAccelZ = sensOut.returnAccelZ;
+		output.returnDistance = sensOut.returnDistance;
+		
 		return output;
 	}
 };
