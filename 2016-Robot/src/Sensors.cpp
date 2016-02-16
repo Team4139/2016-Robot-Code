23a0bb2013b1b80fb404b99c145a528fe543d57a
zhoudex00 <802minzhu@gmail.com>
2016-02-16 15:21:12 -0800

    add ultrasonic

--- 2016-Robot/src/Sensors.cpp
+++ 2016-Robot/src/Sensors.cpp
@@ -3,12 +3,17 @@
 struct Sensors_In
 {
 	bool resetGyro;
+	AnalogInput ultrasonic;
 };
 struct Sensors_Out
 {
 	float returnGyroAngle;
 	float returnAccelX, returnAccelY, returnAccelz;
 	float returnDistance;
+
+	const int ultrasonicChannel = 0; //analog input pin
+	double currentDistance; //distance measured from the ultrasonic sensor values
+	const double valueTocm = 0.125; //factor to convert sensor values to a distance in centimeter
 };
 class Sensors
 {
@@ -18,11 +23,15 @@ public:
 	Sensors()
 	{
 		//you do this
+		ultrasonic(ultrasonicChannel),
 	}
 	Sensors_Out Run(Sensors_In input)
 	{
-		Sensors_Out output;
 
+		currentDistance = (ultrasonic.GetValue()) * valueTocm; //sensor returns a value from 0-4095 that is scaled to inches
+
+		std::cout<<currentDistance <<std::endl;
+		Sensors_Out output;
 		// code here
 
 		return output;
